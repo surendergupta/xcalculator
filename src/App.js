8a3b0bddf4967329fd5e330056e167abad3d1ad4
surendergupta <gupta.surender.1990@gmail.com>
2024-09-04 14:39:59 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleCalculate = (e) => {
+    e.preventDefault();
+    try {
+      const evaluatedResult = evaluateExpression(input);
+      setResult(evaluatedResult);
+    } catch (error) {
+      setResult("Error");
+    }
+  }
+
+  const evaluateExpression = (expression) => {
+    if (/[^0-9+\-*/().\s]/.test(expression)) {
+      throw new Error("Invalid characters in the expression");
+    }
+    try {
+      const result = new Function(`return ${expression}`)();
+      if (isNaN(result)) {
+        setResult(result);
+        // throw new Error("Invalid calculation");
+      }
+      return result;
+    } catch {
+      throw new Error("Invalid calculation");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Calculator</h1>
+      <div className='calculator'>
+        <input type='text' value={input} onChange={(e) => setInput(e.target.value)} />
+        <div id='result' className='result'>{result}</div>
+        <div className='keypad'>
+          <button onClick={() => setInput(input + '7')}>7</button>
+          <button onClick={() => setInput(input + '8')}>8</button>
+          <button onClick={() => setInput(input + '9')}>9</button>
+          <button onClick={() => setInput(input + '+')}>+</button>
+          <button onClick={() => setInput(input + '4')}>4</button>
+          <button onClick={() => setInput(input + '5')}>5</button>
+          <button onClick={() => setInput(input + '6')}>6</button>
+          <button onClick={() => setInput(input + '-')}>-</button>
+          <button onClick={() => setInput(input + '1')}>1</button>
+          <button onClick={() => setInput(input + '2')}>2</button>
+          <button onClick={() => setInput(input + '3')}>3</button>
+          <button onClick={() => setInput(input + '*')}>*</button>
+          <button onClick={() => { setInput(''); setResult(''); }}>C</button>
+          <button onClick={() => setInput(input + '0') }>0</button>
+          <button onClick={handleCalculate}>=</button>
+          <button onClick={() => setInput(input + '/')}>/</button>
+        </div>
+      </div>
     </div>
   );
 }
